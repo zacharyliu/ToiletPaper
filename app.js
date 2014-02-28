b9bf3b11f769a66f9bdf143fb842468fa64e4ff2
Zachary Liu <liu.zachary@gmail.com>
2014-02-28 15:36:16 -0500

    app.js

--- app.js
+++ app.js
@@ -1,15 +1,25 @@
-
-/**
- * Module dependencies.
- */
-
-var express = require('express')
-  , routes = require('./routes');
-
+// Express setup
+var express = require('express');
 var app = module.exports = express.createServer();
 
-// Configuration
+// Johnny-five setup
+var five = require("johnny-five");
+var board = new five.Board();
+var servo = new five.Servo({
+    pin: 9,
+    type: "continuous"
+});
 
+// Twit setup
+var Twit = require('twit');
+var T = new Twit({
+    consumer_key: 'XNICKgCt3HTc5qjLCaVw',
+    consumer_secret: 'dbzZdj35SV0lHSVVIuf6tb8WdeHy2Y1Z1D4mkJLys',
+    access_token: '2343732187-prlOnbKIBqHRoSXNRIw5F9ZtVarJkpfXXK8UKOC',
+    access_token_secret: 'l3qS4ryEWsz8zxPH78aBotoGBgDony2pv8aE8aGSEiWXt'
+});
+
+// Configuration
 app.configure(function(){
   app.set('views', __dirname + '/views');
   app.set('view engine', 'jade');
@@ -27,9 +37,27 @@ app.configure('production', function(){
   app.use(express.errorHandler());
 });
 
+// Functions
+function dispense() {
+    servo.cw(1);
+    setTimeout(function() {
+        servo.stop();
+    }, 2000);
+}
+
 // Routes
+app.get('/', function(req, res){
+    res.render('index', { title: 'Express' })
+});
 
-app.get('/', routes.index);
+app.get('/api/dispense', function(req, res) {
+    dispense();
+    res.send();
+});
 
-app.listen(process.env.PORT || 80);
-console.log("Express server listening on port %d in %s mode", app.address().port, app.settings.env);
+// Initialization
+console.log("Connecting to Arduino");
+board.on("ready", function() {
+    app.listen(process.env.PORT || 3000);
+    console.log("Express server listening on port %d in %s mode", app.address().port, app.settings.env)
+});
