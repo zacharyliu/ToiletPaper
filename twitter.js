6313f72db6c70b0fc8b617d4f1a92c8c3a994139
Zachary Liu <liu.zachary@gmail.com>
2014-03-05 22:47:21 -0500

    Update API keys (read/write access)

--- twitter.js
+++ twitter.js
@@ -1,9 +1,9 @@
 var Twit = require('twit');
 exports.T = new Twit({
-    consumer_key: 'XNICKgCt3HTc5qjLCaVw',
-    consumer_secret: 'dbzZdj35SV0lHSVVIuf6tb8WdeHy2Y1Z1D4mkJLys',
-    access_token: '2343732187-prlOnbKIBqHRoSXNRIw5F9ZtVarJkpfXXK8UKOC',
-    access_token_secret: 'l3qS4ryEWsz8zxPH78aBotoGBgDony2pv8aE8aGSEiWXt'
+    consumer_key: '0T0gt00QFs9N0iYAAKK24Q',
+    consumer_secret: 'k459dPbm2DCI500VRXqDPm3wJaK4ho2SPfQiTshE',
+    access_token: '2343732187-u4yJaigwjmCcUozsqR9V0Y4TR1QFMtyWRZ8E27v',
+    access_token_secret: 'VIDmZxjkpsbr1JEFi4Aj9xLKqkC3wjMwM31lDBcWT32wM'
 });
 
 exports.paperLow = function(callback) {
