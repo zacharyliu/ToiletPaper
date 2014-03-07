189b58f08fbef3051f00d1026fe86d7f9b4ad566
Zachary Liu <liu.zachary@gmail.com>
2014-03-07 02:48:06 -0500

    Sort pins

--- config.js
+++ config.js
@@ -1,13 +1,13 @@
 module.exports = {
     pins: {
-        paperLevelSensor: 7, // Connect both TRIG and ECHO to this pin (see https://github.com/rwaldron/johnny-five/blob/master/docs/ping.md)
         infraredEmitter: 3,
-        infraredDetector: "A0",
+        servoCoins: 6,
+        paperLevelSensor: 7, // Connect both TRIG and ECHO to this pin (see https://github.com/rwaldron/johnny-five/blob/master/docs/ping.md)
+        doorSensor: 8,
         servoDispense: 9,
         servoCutLeft: 10,
         servoCutRight: 11,
-        servoCoins: 12,
-        doorSensor: 2
+        infraredDetector: "A0"
     },
     cameras: {
         coin: 2
